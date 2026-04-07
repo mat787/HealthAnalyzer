8c6fee65618ac9d427828d5f9aa0f060d291857f
mat <01197793@pw.edu.pl>
2026-04-07 23:25:19 +0200

    pliki przykladowe

--- ekg.py
+++ ekg.py
@@ -9,13 +9,17 @@ AMPLITUDE_SCALE = 10 # mm/mV
 # 1. Wczytujemy dane i od razu nadajemy im nazwy kolumn
 ekg_data = pd.read_csv("ekg.csv", skiprows=12, names=['sample', 'voltage'])
 
-cleaned = nk.ecg_clean(ekg_data['voltage'], sampling_rate)
-signals, rpeaks = nk.ecg_peaks(cleaned, sampling_rate)
-nk.ecg_plot(signals, sampling_rate)
+cleaned = nk.ecg_clean(ekg_data['voltage'], sampling_rate=sampling_rate)
+print(cleaned)
+signals, info = nk.ecg_process(ekg_data['voltage'], sampling_rate=sampling_rate)
+nk.ecg_plot(signals, info)
 
 print(ekg_data.head(20))
 
-signal, waves = nk.ecg_delineate(ekg_data, rpeaks, sampling_rate, method="dwt", show=True, show_type='all')
+signal, waves = nk.ecg_delineate(cleaned, info, sampling_rate=sampling_rate, method="dwt", show=True, show_type='all')
+
+import matplotlib.pyplot as plt
+plt.show()
 
 
 
